687805e150368987a5b0d62c2d48128fd14f3428
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-05-09 12:56:42 -0500

    Added lorem ipsum text to show product view with lipsum package.

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    product.show template
+<h1>Product Show</h1>
+<p>Mollit officia ipsum eu anim exercitation do tempor amet adipisicing cupidatat duis non aliquip elit mollit. Lorem tempor occaecat et dolore ullamco id exercitation est aliqua laboris fugiat consequat aute. Quis eu ipsum magna in excepteur mollit
+    eiusmod mollit laboris tempor officia aute excepteur amet nisi. Enim reprehenderit aliqua anim minim sit sint ex cillum. Officia anim cupidatat deserunt aliquip culpa magna do nostrud labore quis. Voluptate amet excepteur esse non aute mollit et
+    excepteur dolor duis aliquip ad proident. Excepteur amet minim nostrud ad est et aute.</p>
+<p>Ullamco ut reprehenderit pariatur fugiat Lorem non nisi. Aliqua veniam voluptate aliqua laboris sit id adipisicing dolore sit. Culpa qui aute ex id reprehenderit ut ad occaecat eu amet quis eiusmod minim Lorem mollit eiusmod. Est id dolore nisi
+    fugiat veniam cillum reprehenderit nulla reprehenderit est nostrud ex Lorem veniam voluptate. Sit minim ullamco cupidatat laboris veniam non non incididunt.</p>
+<p>Veniam laboris laborum ex anim pariatur non est magna et deserunt dolor aliqua nostrud enim. Quis ad minim dolore minim nulla ad veniam nulla. Cupidatat qui et nostrud elit proident et commodo adipisicing ullamco exercitation amet veniam eiusmod
+    dolore enim velit.</p>
+<p>Sit sit sint id ut anim magna dolore labore. Duis id esse incididunt esse sit labore pariatur qui. Nulla ullamco nulla velit cillum esse incididunt mollit eu quis culpa cillum enim. Nulla est qui ad tempor amet sint incididunt cupidatat nostrud et
+    ad laboris velit veniam elit elit irure. Quis ipsum ad adipisicing cillum quis aliqua nostrud laboris veniam cillum tempor. Voluptate aliquip non commodo mollit quis fugiat sit proident. Pariatur eiusmod sit reprehenderit adipisicing deserunt
+    aliquip sunt occaecat qui irure deserunt.</p>
 @endsection
