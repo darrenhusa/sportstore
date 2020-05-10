2473ffe97e3536d9d5effc2707caf72bedf85f31
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-05-10 11:45:20 -0500

    Cleaned-up product seeder to use an array of arrays.

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -11,59 +11,47 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('products')->insert(
+      DB::table('products')->insert([
         ['name' => 'Kayak',
          'description' => 'A boat for one person',
          'category' => 'Watersports',
-         'price' => 275]
-       );
-       DB::table('products')->insert(
-         ['name' => 'Lifejacket',
-          'description' => 'Protective and fashionable',
-          'category' => 'Watersports',
-          'price' => 48.95]
-        );
-        DB::table('products')->insert(
-          ['name' => 'Soccer Ball',
-           'description' => 'FIFA-approved size and weight',
-           'category' => 'Soccer',
-           'price' => 19.50]
-         );
-         DB::table('products')->insert(
-           ['name' => 'Corner Flags',
-            'description' => 'Give your playing field a professional touch',
-            'category' => 'Soccer',
-            'price' => 34.95]
-          );
-          DB::table('products')->insert(
-            ['name' => 'Stadium',
-             'description' => 'Flat-packed 35,000-seat stadium',
-             'category' => 'Soccer',
-             'price' => 79500]
-           );
-           DB::table('products')->insert(
-             ['name' => 'Thinking Cap',
-              'description' => 'Improve brain efficiency by 75%',
-              'category' => 'Chess',
-              'price' => 16]
-            );
-            DB::table('products')->insert(
-              ['name' => 'Unsteady Chair',
-               'description' => 'Secretly give your opponent a disadvantage',
-               'category' => 'Chess',
-               'price' => 29.95]
-             );
-             DB::table('products')->insert(
-               ['name' => 'Human Chess Board',
-                'description' => 'A fun game for the family',
-                'category' => 'Chess',
-                'price' => 75]
-              );
-              DB::table('products')->insert(
-                ['name' => 'Bling-Bling King',
-                 'description' => 'Gold-plated, diamond-studded King',
-                 'category' => 'Chess',
-                 'price' => 1200]
-               );
+         'price' => 275],
+        ['name' => 'Lifejacket',
+         'description' => 'Protective and fashionable',
+         'category' => 'Watersports',
+         'price' => 48.95],
+        ['name' => 'Soccer Ball',
+         'description' => 'FIFA-approved size and weight',
+         'category' => 'Soccer',
+         'price' => 19.50],
+        ['name' => 'Corner Flags',
+         'description' => 'Give your playing field a professional touch',
+         'category' => 'Soccer',
+         'price' => 34.95],
+        ['name' => 'Stadium',
+         'description' => 'Flat-packed 35,000-seat stadium',
+         'category' => 'Soccer',
+         'price' => 79500],
+        ['name' => 'Stadium',
+         'description' => 'Flat-packed 35,000-seat stadium',
+         'category' => 'Soccer',
+         'price' => 79500],
+        ['name' => 'Thinking Cap',
+         'description' => 'Improve brain efficiency by 75%',
+         'category' => 'Chess',
+         'price' => 16],
+        ['name' => 'Unsteady Chair',
+         'description' => 'Secretly give your opponent a disadvantage',
+         'category' => 'Chess',
+         'price' => 29.95],
+        ['name' => 'Human Chess Board',
+         'description' => 'A fun game for the family',
+         'category' => 'Chess',
+         'price' => 75],
+        ['name' => 'Bling-Bling King',
+         'description' => 'Gold-plated, diamond-studded King',
+         'category' => 'Chess',
+         'price' => 1200]
+       ]);
     }
 }
