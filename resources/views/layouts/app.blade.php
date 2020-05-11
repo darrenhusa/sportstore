ac7e61e8846a0064018d1a9d3f1d1160fe4756f9
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-05-10 19:24:56 -0500

    Added bootstrap formatting to layout template and to product index view.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,19 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Sportstore</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-      <div>
-        @yield('content')
+      <div class="navbar navbar-inverse bg-inverse" role="navigation">
+        <a class="navbar navbar-inverse bg-inverse" href="#">SPORTS STORE</a>
+      </div>
+      <div class="row m-1 p-1">
+        <div id="categories" class="col-3">
+          Put something useful here later
+        </div>
+        <div class="col-9">
+          @yield('content')
+        </div>
       </div>
     </body>
 </html>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,14 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Product Index</h1>
-    <div>
-      @foreach($products as $product)
-        <h3>{{ $product->name }}</h3>
-        <p>{{ $product->description }}</p>
-        <h4>{{ $product->price }}</h4>
-      @endforeach
-    </div>
 
+  @foreach($products as $product)
+    <div class="card card-outline-primary m-1 p-1">
+      <div class="bg-faded p-1">
+        <h4>
+          {{ $product->name }}
+          <span class="badge badge-pill badge-primary" style="float:right">
+            <small>{{ $product->price }}</small>
+          </span>
+        </h4>
+      </div>
+      <div class="card-text p-1">{{ $product->description }}</div>
+    </div>
+  @endforeach
+
+  <div class="">
     {{ $products->links() }}
+  </div>
 @endsection
